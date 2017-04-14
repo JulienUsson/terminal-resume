f4338899a71eda0986ee904d7a51d20de440861d
Julien USSON <usson.julien@gmail.com>
2017-04-14 11:43:11 +0200

    init of the project with minimum functions

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <terminal></terminal>
+</template>
+
+<script>
+import Terminal from './components/Terminal';
+
+export default {
+  name: 'app',
+  components: {
+    Terminal,
+  },
+};
+</script>
+
+<style>
+ html, body {
+   margin: 0;
+   height: 100%;
+ }
+</style>
---
+++ src/components/Terminal.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="terminal"
+       @click="focusCommandInput">
+    <input id="command"
+           type="text"
+           ref="command"
+           v-model="command" /> {{command}}
+    <Caret></Caret>
+  </div>
+</template>
+
+<script>
+import Caret from './Caret';
+
+export default {
+  data: () => ({
+    content: 'content',
+    command: 'command',
+    disabled: false,
+  }),
+  methods: {
+    focusCommandInput() {
+      this.$refs.command.focus();
+    },
+  },
+  components: {
+    Caret,
+  },
+};
+</script>
+
+<style scoped>
+#terminal {
+  min-height: 100%;
+  width: 100%;
+  background-color: black;
+  color: white;
+  font-weight: bold;
+  font-size: 16px;
+}
+
+#command {
+  position: absolute;
+  opacity: 0;
+  height: 0;
+  width: 0;
+}
+</style>

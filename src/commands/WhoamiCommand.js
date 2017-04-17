f71f256aeca098eb0fe4e773f3aa2982fd0ba60f
Julien USSON <usson.julien@gmail.com>
2017-04-17 22:07:37 +0200

    remove ascii art name in the whoami command

--- src/commands/WhoamiCommand.js
+++ src/commands/WhoamiCommand.js
@@ -1,9 +1,5 @@
 function WhoamiCommand() {
-  return `     ____.     .__  .__                 ____ ___                            
-    |    |__ __|  | |__| ____   ____   |    |   \\______ __________   ____   
-    |    |  |  \\  | |  |/ __ \\ /    \\  |    |   /  ___//  ___/  _ \\ /    \\  
-/\\__|    |  |  /  |_|  \\  ___/|   |  \\ |    |  /\\___ \\ \\___ (  <_> )   |  \\ 
-\\________|____/|____/__|\\_____>___|__/ |______//______>______>____/|___|__/ 
+  return `Julien Usson
 Développeur Web Full Stack
 #Java #JavaScript #Angular #VueJS #React
 Clermont-Ferrand, France`;
